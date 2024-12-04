ac51a06be5a1c0288613345344edae42c66e8919
ssganesh <ssganesh035@gmail.com>
2024-12-04 23:33:43 +0530

    Initial commit

---
+++ tests/beetest.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require('@playwright/test');
+
+test('Click on Add A Mock Rule in Beeceptor', async ({ page }) => {
+  // 1. Navigate to Beeceptor
+  await page.goto('https://app.beeceptor.com/console/beetest');
+  await page.waitForLoadState('domcontentloaded');
+
+  const createMockApiSelector = '//h4[contains(@class, "task-title") and text()="Create a Mock API"]';
+  await page.waitForSelector(createMockApiSelector);
+
+  // Click on "Create a Mock API" title
+  await page.click(createMockApiSelector);
+
+  const addMockRuleButtonSelector = '//div[@id="details-1"]//button[contains(@class, "action-button")]';
+  await page.waitForSelector(addMockRuleButtonSelector);
+
+  await page.click(addMockRuleButtonSelector);
+
+  const addNextButton = '//div[@class="driver-popover-footer"]//button[contains(@class, "driver-popover-next-btn")]';
+
+  await page.waitForSelector(addNextButton);
+  await page.click(addNextButton);
+
+  await page.locator('body').click();
+
+  const addRuleTypes = '//div[@class="btn-group"]//button[contains(@class, "btn btn-default dropdown-toggle")]';
+
+  await page.waitForSelector(addRuleTypes);
+  await page.click(addRuleTypes);
+
+  const addProxy = '//*[@id="rulesList"]/div[2]/div/div[1]/div/ul/li[2]/a';
+
+  await page.waitForSelector(addProxy);
+  await page.click(addProxy);
+
+  
+});
